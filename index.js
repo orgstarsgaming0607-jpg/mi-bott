ba13b472bfc7469c8fe324db02bfff8bd311c764
orgstarsgaming0607-jpg <orgstarsgaming0607@gmail.com>
2026-09-01 16:53:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,90 @@ client.on('messageCreate', async message => {
 
     const command = message.content.trim().toLowerCase();
 
+    // !Normativa
+if (message.content.toLowerCase() === '!normativa') {
+    return message.channel.send(
+`# 📜 NORMATIVA DEL TORNEO
+
+🤖 **Hola, soy LUX, el bot oficial de EOS.**
+
+Antes de participar en el torneo, es **obligatorio leer y conocer la normativa**.
+
+📖 **Consulta la normativa completa aquí:**
+👉 [https://docs.google.com/document/d/1KC8kQPGwtOhT-A3qClu1qLW_ENs_O_7_BDVybgmj4Mc/edit?usp=sharing]
+
+⚠️ **IMPORTANTE**
+
+El incumplimiento de la normativa, así como realizar acciones que perjudiquen el correcto funcionamiento del torneo, puede conllevar diferentes sanciones.
+
+Las sanciones pueden ir desde:
+🟡 **Avisos**
+🟠 **Sanciones**
+🔴 **Expulsión del torneo**
+
+La organización se reserva el derecho de aplicar la sanción correspondiente dependiendo de la gravedad de cada situación.
+
+🤖 **Si participas en el torneo, aceptas y te comprometes a cumplir la normativa.**`
+    );
+}
+
+
+// !preins
+if (message.content.toLowerCase() === '!preins') {
+    return message.channel.send(
+`# 📝 PRE-INSCRIPCIÓN AL TORNEO
+
+🤖 **Hola, soy LUX, el bot oficial de EOS.**
+
+Para realizar la **pre-inscripción** al torneo es necesario completar correctamente el formulario.
+
+📋 **FORMULARIO DE PRE-INSCRIPCIÓN**
+👉 [https://forms.gle/tFNgvfan63tDdmzu5]
+
+⚠️ **IMPORTANTE**
+
+La persona que realice la pre-inscripción y sea el **contacto del equipo debe estar dentro del servidor de Discord de EOS**.
+
+Si la persona de contacto no se encuentra dentro del servidor, la pre-inscripción podrá no ser válida.
+
+✅ Una vez rellenado el formulario, esperad las indicaciones de la organización.
+
+🤖 **Gracias por confiar en EOS. ¡Nos vemos en el torneo!**`
+    );
+}
+
+
+// !pedirol
+if (message.content.toLowerCase() === '!pedirol') {
+    return message.channel.send(
+`# 🚨 SOLICITUD DE ROLES 🚨
+
+🤖 **Hola, soy LUX, el bot oficial de EOS.**
+
+## ⚠️ MUY IMPORTANTE ⚠️
+
+**SI NO SOLICITAS TU ROL EN ESTE CANAL, NO PODRÁS PARTICIPAR EN EL TORNEO UNA VEZ ESTE HAYA COMENZADO.**
+
+📌 Para solicitar los roles de vuestro equipo, escribid vuestro nombre y el rol correspondiente siguiendo este formato:
+
+**Nombre equipo:**
+**Rol:**
+
+### 📋 EJEMPLO
+
+**Nombre equipo:** EOS Academy
+**Rol:** Duelista
+
+⚠️ **Cada jugador debe solicitar su rol antes de que comience el torneo.**
+
+Una vez iniciado el torneo, **no se aceptarán solicitudes de roles para poder participar**, salvo que la organización indique lo contrario.
+
+🤖 **Revisad bien los datos antes de enviarlos.**
+
+¡Gracias y mucha suerte en el torneo! 🎮🔥`
+    );
+}
+    
     // !post
 
     if (command.startsWith('!post ')) {
